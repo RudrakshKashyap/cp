ca8b60cfacba1d050a37856787c9c47510f71399
Rudraksh Kashyap <200rudra@gmail.com>
2020-04-28 19:13:02 +0530

    Update and rename note.txt to note.cpp

--- note.cpp
+++ note.cpp
@@ -5,6 +5,13 @@ __builtin_popcount(x);
 __builtin_clz(x)
 __builtin_ctz(x)
 cpp produce negitive modulo but python doesnt ((x % 5) + 5) % 5
+setw(5) ____1 //mimimum width 5
+setfill('f') ffff1
+setprecision(10)
+fixed	// 0.100
+scientific // 1.000e-001
+cout.precision(17);
+round(x);
 
 strchr(str,'a'); used for finding occurrence of a character in a string.
 str = to_string(a);
@@ -244,8 +251,171 @@ if (num & 1)
 else
    cout << "EVEN";
 
-//  Use of strlen() can be avoided by:
+ Use of strlen() can be avoided by:
 for (i=0; s[i]; i++)
 {
 }
-// loop breaks when the character array ends.
+loop breaks when the character array ends.
+
+
+
+/*
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        long lw=INT_MIN;
+        long hg=INT_MAX;
+        while(lw<=hg)
+        {
+            long mid=(lw+hg)/2;
+            int ct=0,e=0;
+            for(auto i:nums)
+            {
+                if(i>mid)
+                    ct++;
+                else if(i==mid)
+                    e++;
+            }
+            if(ct<k&&ct+e>=k)
+            {
+                return mid;
+            }
+            else if(ct+e<k)
+            {
+                hg=mid-1;
+            }
+            else
+            {
+                lw=mid+1;
+            }
+
+        }
+        return 0;
+    }
+};
+Number of Digtits in base b = 1 + floor( log b (N) )
+Example :
+In base 10 ,N=1430 ,
+here number of digits=1+floor(3.15)=4
+
+a std::map has a BidirectionalIterator. A BidirectionalIterator does not support random access and can only be incremented or decremented.
+
+
+
+https://ideone.com/SMIZgR -- max xor pair
+
+string s;
+cin>>s; // Suppose s = "100100101"
+int n = stoi(s, 0, 2);
+cout<<n; // 293
+
+
+https://github.com/williamfiset/Algorithms/blob/master/src/main/java/com/williamfiset/algorithms/graphtheory/EulerianPathDirectedEdgesAdjacencyList.java
+or see the code at the last of the file
+
+ ffs() function: ffs() function returns the index of first least significant set bit.
+int clz(int N) {
+    return N ? 32 - __builtin_clz(N) : -inf;
+}
+
+lcm=a*b/__gcd(a,b);
+
+
+
+
+// nCr
+ll facinv[600000];
+ll inv[600000];
+ll fac[600000];
+fac[0]=1; facinv[0]=1;
+inv[1]=1; fac[1]=1; facinv[1]=1;
+for(i=2 ; i<=599999 ; i++)
+{
+	fac[i]=(i*fac[i-1])%mod;
+	inv[i]=((mod-mod/i)*inv[mod%i])%mod;
+	facinv[i]=(facinv[i-1]*inv[i])%mod;
+}
+
+
+
+
+
+
+
+
+
+
+https://codeforces.com/contest/723/problem/E
+
+#include <bits/stdc++.h>
+
+#define fc first
+#define sc second
+#define all(x) x.begin(), x.end()
+#define bit(x) __builtin_popcount(x)
+#define pb push_back
+#define popb pop_back
+#define ll long long
+
+using namespace std;
+
+const int N = (int) 200 + 7;
+int d[N];
+int d_in[N], d_out[N];
+multiset <int> g[N];
+vector <pair <int, int> > go;
+
+void dfs(int v) {
+    while (g[v].size() > 0) {
+        int to = *g[v].begin();
+        go.pb({v, to});
+        g[to].erase(g[to].find(v));
+        g[v].erase(g[v].begin());
+        dfs(to);
+    }
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n, m;
+        scanf("%d%d", &n, &m);
+        memset(d, 0, sizeof d);
+        memset(d_in, 0, sizeof d_in);
+        memset(d_out, 0, sizeof d_out);
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            u--, v--;
+            d[u]++, d[v]++;
+            g[u].insert(v);
+            g[v].insert(u);
+        }
+        int t = n;
+        for (int i = 0; i < n; i++) {
+            if (d[i] % 2 == 1) {
+                g[i].insert(t);
+                g[t].insert(i);
+            }
+        }
+        go.clear();
+        for (int i = 0; i <= n; i++) dfs(i);
+        vector <pair <int, int> > res;
+        for (auto f : go) {
+            if (f.fc != t && f.sc != t) {
+                d_out[f.fc]++;
+                d_in[f.sc]++;
+                res.pb(f);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) ans += (d_in[i] == d_out[i]);
+        printf("%d\n", ans);
+        for (auto f : res) {
+            printf("%d %d\n", f.fc + 1, f.sc + 1);
+        }
+    }
+}
