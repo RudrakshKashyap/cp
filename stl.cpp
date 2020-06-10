9a2c3bf36c164e8a8df83967e4a82a39778c6c43
Rudraksh Kashyap <200rudra@gmail.com>
2020-06-10 15:05:36 +0530

    Create stl.cpp

---
+++ stl.cpp
@@ -0,0 +1,39 @@
+__builtin_popcount(x); __builtin_popcountll(x);
+__builtin_clz(x)
+__builtin_ctz(x)
+cpp produce negitive modulo but python doesnt ((x % 5) + 5) % 5
+setw(5) ____1 //mimimum width 5
+setfill('f') ffff1
+setprecision(10)
+fixed	// 0.100
+scientific // 1.000e-001
+cout.precision(17);
+round(x);
+rotate(vec.begin(), vec.begin() + k, vec.end()); //cyclic left shift of k
+int a = std::numeric_limits<int>::max();
+priority_queue<int,vector<int>,greater<int>> pq ; min heap
+If you want to print, say, last 20 bits of a numbercout << bitset<20>(n) << "\n";
+string rev = string(str.rbegin(),str.rend());
+
+//it also works for array,vectors ,list etc
+string s="123"
+do {
+	cout<<s<<'\n';
+} while(next_permutation(s.begin(),s.end()));
+//123
+//132
+//213
+.. till //321 after that it will return false
+
+
+strchr(str,'a'); used for finding occurrence of a character in a string.
+str = to_string(a);
+a = stoi(str);
+it=itreatrator of map---- it->first,it->second
+vec.size(); vec.capacity(); vec.reserve(1000);
+deque<int> dq; dq.push_front(5);
+queue<int>qu; print(qu);
+stack<int> stk;
+priority_queue<int> q; min max heap
+p/q mod m = (p*(q^m-2))%m
+~(-1)=0
