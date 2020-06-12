1b3f9df8c5b675c8d67eafcbc4d3e3263a3ea109
Rudraksh Kashyap <200rudra@gmail.com>
2020-06-12 12:41:52 +0530

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,26 @@
+https://codeforces.com/blog/entry/55782?#comment-395139
+//dont know if this code is correct, its just for an idea
+intt trie[maxx][26], finish[maxx];
+intt nxt = 1;
+void Add (string s){
+    intt node = 0;
+    for (intt i = 0; s[i] != '\0'; i++) {
+        if(trie[node][s[i] - 'a'] == 0) {
+            node = trie[node][s[i] - 'a'] = nxt;
+            nxt++;
+        } else {
+            node = trie[node][s[i] - 'a'];
+        }
+    }
+    finish[nxt - 1] = 1;
+}
+
+intt Find (string s) {
+    intt idx = 0;
+    for (intt i = 0; i < s.size(); i++)
+        if (trie[idx][s[i] - 'a'] == 0)
+            return 0;
+        else
+            idx = trie[idx][s[i] - 'a'];
+    return finish[idx];
+}
