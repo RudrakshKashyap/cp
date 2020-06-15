075398cb9623916713ec93add292089190f718d5
Rudraksh Kashyap <200rudra@gmail.com>
2020-06-15 19:31:21 +0530

    Update ford_fulkerson.cpp

--- ford_fulkerson.cpp
+++ ford_fulkerson.cpp
@@ -2,3 +2,75 @@ all the implementation below are nice
 https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
 https://cp-algorithms.com/graph/edmonds_karp.html
 https://paste.ubuntu.com/7194906/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long MF;
+long long f[1000][1000];
+long long c[1000][1000];
+int timer;
+int p[10000];
+
+int n, m;
+
+int used[1000];
+
+const int inf = (int)(1e9);
+
+int s, t;
+
+bool bfs (int v, int t) {
+	queue <int> q;
+	q.push (v);
+
+	while (!q.empty ()) {
+		int x = q.front();
+		q.pop();
+		if (x == t)
+			return true;
+		for (int i = 1; i <= n; i++) {
+			if (used[i] != timer && c[x][i] > f[x][i]) {
+				p[i] = x;
+				used[i] = timer;
+				q.push (i);
+			}
+		}
+	}
+
+	return false;
+}
+
+int main () {
+	freopen ("input.txt", "r", stdin);
+	freopen ("output.txt", "w", stdout);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int x, y, w; cin >> x >> y >> w;
+		c[x][y] = w;
+	}
+	
+	s = 1;
+	t = n;
+
+	int x = inf;
+	timer++;
+	while (bfs (s, t)) {
+		timer++;
+		long long mn = inf;
+		for (int i = t; i != s; i = p[i])
+			mn = min (mn, c[p[i]][i] - f[p[i]][i]);
+		
+		for (int i = t; i != s; i = p[i])
+			f[p[i]][i] += mn,
+			f[i][p[i]] -= mn;
+		
+		MF += mn;
+		memset (p, 0, sizeof p);
+	}
+	cout << MF;
+
+	return 0;
+}
