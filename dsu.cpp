43c2de18e03e761309c530a1692362fc252ca43f
Rudraksh Kashyap <200rudra@gmail.com>
2020-06-17 21:43:02 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -14,3 +14,19 @@ bool u(int x,int y){
     T[x] = y;
     return 1;
 }
+
+int T[35];
+int fi(int nod){
+    if(nod==T[nod]){
+        return nod;
+    }
+    return T[nod] = fi(T[nod]);
+}
+rep(i,n+1) T[i]=i;
+rep(i,m)
+{
+    int x,y;
+    cin>>x>>y;
+    int p=fi(x),q=fi(y);
+    T[p]=q;
+}
