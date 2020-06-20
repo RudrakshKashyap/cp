fb60f3bc32f0cea79764bff3b1b4fa29558b1a7b
Rudraksh Kashyap <200rudra@gmail.com>
2020-06-20 13:57:25 +0530

    Create convexhull.cpp

---
+++ convexhull.cpp
@@ -0,0 +1,32 @@
+template <class F> using Polygon = vector<Point<F>>;
+inline int prev(int i, int n) { return i == 0 ? n-1 : i-1; }
+inline int next(int i, int n) { return i == n-1 ? 0 : i+1; }
+
+template <class F>
+F area(const Polygon<F>& poly) {
+  int n = static_cast<int>(poly.size());
+  F area = F(0);
+  for (int i = 0; i < n; ++i)
+    area += poly[i].x * (poly[next(i, n)].y - poly[prev(i, n)].y);
+  return area;
+}
+
+template <class F>
+Polygon<F> convexHull(Polygon<F> points) {
+  sort(begin(points), end(points));
+  Polygon<F> hull;
+  hull.reserve(points.size() + 1);
+  for (int phase = 0; phase < 2; ++phase) {
+    auto start = hull.size();
+    for (auto& point : points) {
+      while (hull.size() >= start+2 &&
+             ccw(point, hull.back(), hull[hull.size()-2]) <= 0)
+        hull.pop_back();
+      hull.push_back(point);
+    }
+    hull.pop_back();
+    reverse(begin(points), end(points));
+  }
+  if (hull.size() == 2 && hull[0] == hull[1]) hull.pop_back();
+  return hull;
+}
