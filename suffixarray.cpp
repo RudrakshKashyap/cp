5df28aa1748074a92d9238529abb1a0ce4ebfb17
Rudraksh Kashyap <200rudra@gmail.com>
2020-07-01 14:32:38 +0530

    Update suffixarray.cpp

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -151,3 +151,21 @@ vector<int> sa(string s)
 
     return p;
 }
+
+
+// kasai algo
+Let's look at two adjacent suffixes in the sorted order (order of the suffix array). Let their starting positions be i and j and their lcp equal to k>0. If we remove the first letter of both suffixes - i.e. we take the suffixes i+1 and j+1 - then it should be obvious that the lcp of these two is k−1. However we cannot use this value and write it in the lcp array, because these two suffixes might not be next to each other in the sorted order. The suffix i+1 will of course be smaller than the suffix j+1, but there might be some suffixes between them. However, since we know that the LCP between two suffixes is the minimum value of all transitions, we also know that the LCP between any two pairs in that interval has to be at least k−1, especially also between i+1 and the next suffix. And possibly it can be bigger.
+
+
+     
+if suffix of index i comes before j in SA(and their lcp>0) 
+then suffix of index i+1 will come before suffix of index j+1 because when we compared suffix of index i and j
+we found that their first char is same(bc lcp>0) so ofcourse we have compared them with their next characters
+
+suffix of index i+1
+some other suffix --- will also have (k-1) char common
+some other suffix
+some other suffix
+some other suffix
+suffix of index j+1 --- this have k-1 char common
+
