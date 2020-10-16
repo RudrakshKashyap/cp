85db2a102a03525d7176d8e9dc034008119f70ee
Rudraksh Kashyap <200rudra@gmail.com>
2020-10-16 18:26:12 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -68,3 +68,60 @@ void Delete(string s)
     }
     finish[node - 1]--;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////leetcode
+class Trie {
+    struct Trienode
+    {
+        Trienode* next[26];
+        bool is_word = false;
+    };
+public:
+    Trienode *root;
+    
+    
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Trienode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string s) {
+        Trienode* p = root;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(p -> next[s[i] - 'a'] == NULL)
+                p -> next[s[i] - 'a'] = new Trienode();
+            
+            p = p -> next[s[i] - 'a'];
+        }
+        p -> is_word = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trienode* p = root;
+        for(int i = 0; i < word.size() && p != NULL; i++)
+            p = p -> next[word[i] - 'a'];
+        
+        return p!=NULL && p -> is_word;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string word) {
+        Trienode* p = root;
+        for(int i = 0; i < word.size() && p != NULL; i++)
+            p = p -> next[word[i] - 'a'];
+        
+        return p!=NULL;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
