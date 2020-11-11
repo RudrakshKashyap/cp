03de3b73c1d4fb8d141312688bba7cc9c27e7f38
Rudraksh Kashyap <200rudra@gmail.com>
2020-11-11 14:08:04 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -5,7 +5,7 @@ ll inv[600000];
 ll fac[600000];
 fac[0]=1; facinv[0]=1;
 inv[1]=1; fac[1]=1; facinv[1]=1;
-for(i=2 ; i<=599999 ; i++)
+for(int i=2 ; i<=599999 ; i++)
 {
 	fac[i]=(i*fac[i-1])%mod;
 	inv[i]=((mod-mod/i)*inv[mod%i])%mod;
