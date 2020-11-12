da32a35d049598b94f5c8d951dfcdfb404630c6d
Rudraksh Kashyap <200rudra@gmail.com>
2020-11-12 18:45:54 +0530

    Update sparse.cpp

--- sparse.cpp
+++ sparse.cpp
@@ -1,10 +1,10 @@
 const int k = 16;   //maxlog floor
-const int N = 1e5;  //maxn
+const int maxn = 1e5;  //maxn
 
-int n;
-long long table[N][k + 1]; // k + 1 because we need to access table[r][MAXLOG]
-int Arr[N];
-int numlog[N+1];
+int n;  //size of arr
+long long table[maxn][k + 1]; // k + 1 because we need to access table[r][MAXLOG]
+int arr[maxn];
+int numlog[maxn+1];
 
 
 void build()
@@ -15,7 +15,7 @@ void build()
 
     // build Sparse Table
     for(int i = 0; i < n; i++)
-        table[i][0] = Arr[i];
+        table[i][0] = arr[i];
     for(int j = 1; j <= numlog[n]; j++)
     {
         for(int i = 0; i <= n - (1 << j); i++)
@@ -29,17 +29,17 @@ int getMax(int l, int r)    //these l and r are inclusive [l,r]
     return max(table[l][curlog], table[r - (1 << curlog) + 1][curlog]);
 }
 
-long long query(int L, int R) 
+long long query(int l, int r) 
 { 
     // boundaries of next query, 0-indexed 
     long long answer = 0; 
     for (int j = numlog[n]; j >= 0; j--) { 
-        if (L + (1 << j) - 1 <= R) { 
-            answer = answer + table[L][j]; 
+        if (l + (1 << j) - 1 <= r) { 
+            answer = answer + table[l][j]; 
   
             // instead of having L', we 
             // increment L directly 
-            L += 1 << j; 
+            l += 1 << j; 
         } 
     } 
     return answer; 
