590953b83c6e0721f1a59cf8496eb2b3713c1797
Rudraksh Kashyap <200rudra@gmail.com>
2020-12-12 13:11:34 +0530

    Update func in main.cpp

--- func in main.cpp
+++ func in main.cpp
@@ -1,8 +1,44 @@
 vector<bool> mark(Alp);
-function<void(int)> dfs = [&](int u)
+function<void(int)> dfs = [&](int u)    // [&] means passing by reference
 {
     mark[u] = true;
     for (auto v : adj[u])
         if (!mark[v])
             dfs(v);
 };
+
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun>
+class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T>
+    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+ 
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+ 
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+y_combinator([&](auto recurse, int start, int end) -> void {
+        if (start >= end)
+            return;
+ 
+        if (!done.emplace(start, end).second)
+            return;
+ 
+        sums.insert(prefix_sum[end] - prefix_sum[start]);
+        int minimum = A[start];
+        int maximum = A[end - 1];
+        int cutoff = (minimum + maximum) / 2;
+        int split = int(upper_bound(A.begin(), A.end(), cutoff) - A.begin());
+        recurse(start, split);
+        recurse(split, end);
+    })(0, N);
