f1e6b39192f753a60ab4a80771d67315074675df
Rudraksh Kashyap <200rudra@gmail.com>
2020-12-16 13:57:34 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,14 +1,22 @@
 // nCr
 //proof at https://cp-algorithms.com/algebra/module-inverse.html
-ll facinv[600000];
-ll inv[600000];
-ll fac[600000];
-fac[0]=1; facinv[0]=1;
-inv[1]=1; fac[1]=1; facinv[1]=1;
-for(int i=2 ; i<=599999 ; i++)
+const int maxn = 6e5;
+ll facinv[maxn];
+ll inv[maxn];
+ll fac[maxn];
+
+void compute()
 {
-	fac[i]=(i*fac[i-1])%mod;
-	inv[i]=((mod-mod/i)*inv[mod%i])%mod;
-	facinv[i]=(facinv[i-1]*inv[i])%mod;
+    fac[0] = 1, fac[1] = 1;
+    facinv[0] = 1, facinv[1] = 1;
+    inv[1] = 1;
+    for(int i=2 ; i < maxn ; i++)
+    {
+    	fac[i] = (i * fac[i-1]) % mod;
+    	inv[i] = ((mod - mod/i) * inv[mod%i]) % mod;
+    	facinv[i] = (facinv[i-1] * inv[i]) % mod;
+    }
 }
+
+long long nCr(int n, int r) { return ((fac[n] * facinv[r])%mod * facinv[n-r]) % mod;}
 //lucas theom proof https://cp-algorithms.com/algebra/factorial-modulo.html
