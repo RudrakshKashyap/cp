c856b5b63adf284d8ff7c360bdf81e0159577cd3
Rudraksh Kashyap <200rudra@gmail.com>
2020-12-16 16:50:53 +0530

    Create cc.cpp

---
+++ cc.cpp
@@ -0,0 +1,10 @@
+int n = a.size();
+vector<pair<int, int>> pairs(n);
+for(int i = 0; i < n; ++i) pairs[i] = {a[i], i};
+sort(pairs.begin(), pairs.end());
+int nxt = 0;
+for(int i = 0; i < n; ++i)
+{
+	if(i > 0 && pairs[i-1].first != pairs[i].first) nxt++;
+	a[pairs[i].second] = nxt; //changing the values of actual array
+}
