58ec2862042cc84263c81e27cd417b3badc281c0
Rudraksh Kashyap <200rudra@gmail.com>
2020-12-17 19:41:18 +0530

    Update note.cpp

--- note.cpp
+++ note.cpp
@@ -405,3 +405,20 @@ int main() {
         }
     }
 }
+
+Any positive number n is expressible as a sum of two squares if and only if the prime factorization of n, 
+every prime of the form (4k+3) occurs an even number of times.
+	public boolean judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
+    }
