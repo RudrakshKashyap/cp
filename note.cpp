98f18aa84b2aed7e45d20b75d511e382d7cf0375
Rudraksh Kashyap <200rudra@gmail.com>
2021-01-01 14:33:09 +0530

    Update note.cpp

--- note.cpp
+++ note.cpp
@@ -3,19 +3,9 @@ factor command on linux
 oeis.org
 https://www.desmos.com/
 file-header/lib/templates i did this command mv C.tmpl C.tmpl_original
-__builtin_popcount(x); __builtin_popcountll(x);
-__builtin_clz(x)
-__builtin_ctz(x)
-cpp produce negitive modulo but python doesnt ((x % 5) + 5) % 5
-setw(5) ____1 //mimimum width 5
-setfill('f') ffff1
-setprecision(10)
-fixed	// 0.100
-scientific // 1.000e-001
-cout.precision(17);
-round(x);
+
+
 int a = std::numeric_limits<int>::max();
-priority_queue<int,vector<int>,greater<int>> pq ; min heap
 
 //it also works for array,vectors ,list etc
 string s="123"
@@ -36,7 +26,6 @@ vec.capacity(); vec.reserve(1000);
 
 queue<int>qu; print(qu);
 
-priority_queue<int> q; min max heap
 
 ~(-1)=0
 lower_bound(arr,arr+n,35)-arr;
@@ -86,130 +75,6 @@ vector<int> getpath(int a, int b)
 
 
 
-binary lifting
-
-int f[maxn][logN], depth[maxn];
-
-//this code for dfs is only for tree, you should use bool visited array for graph
-void dfs(int v, int p, int d)
-{
-    depth[v]=d;
-    for (int i = 1; i < logN; i++)
-        p[v][i] = p[p[v][i - 1]][i - 1];
-    for(int c: g[v])
-        if(c != p) {
-            p[c][0] = v;
-            dfs(c,v,d+1,val);
-        }
-}
-int LCA (int u, int v) {
-    if (depth[u] < depth[v]) swap(u, v);
-
-    for (int i = logN - 1; i >= 0; i--)
-        if (depth[p[u][i]] >= depth[v]) {
-            u = p[u][i];
-        }
-    // int d = depth[u]-depth[v];
-    // while(d>0)
-    // {
-    //     int i=log2(d);
-    //     u=p[u][i];
-    //     d -= (1<<i);
-    // }
-
-    if (u == v) return u;
-
-    for (int i = logN - 1; i >= 0; i--)
-        if (p[u][i] != p[v][i]) {
-            u = p[u][i];
-            v = p[v][i];
-        }
-    // while(p[u][0]!=p[v][0])
-    // {
-    //     u=p[u][0];
-    //     v=p[v][0];
-    // }
-
-    return p[u][0];
-}
-
-
-
-//bfs
-vector<vector<int>> adj;  // adjacency list representation
-int n; // number of nodes
-int s; // source vertex
-
-queue<int> q;
-vector<bool> used(n);
-vector<int> d(n), p(n);
-
-q.push(s);
-used[s] = true;
-p[s] = -1;
-while (!q.empty()) {
-    int v = q.front();
-    q.pop();
-    for (int u : adj[v]) {
-        if (!used[u]) {
-            used[u] = true;
-            q.push(u);
-            d[u] = d[v] + 1;
-            p[u] = v;
-        }
-    }
-}
-
-
-//sparse table
-
-void buildSparseTable(int M[MAXN][LOGMAXN], int A[MAXN], int N)
-{
-
-    int i, j;
-
-    //initialize M for the intervals with length 1
-    for (i = 0; i < N; i++) M[i][0] = i;
-
-    //compute values from smaller to bigger intervals
-    for (j = 1; 1 << j <= N; j++)
-        for (i = 0; i + (1 << j) - 1 < N; i++)
-            if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]])
-                M[i][j] = M[i][j - 1];
-            else
-                M[i][j] = M[i + (1 << (j - 1))][j - 1];
-}
-
-class SparseTable {
-private:
-	int **M, N, LOGN, *A;
-public:
-	SparseTable(int N_, int A_[]) {
-		N = N_, A = A_, LOGN = ceil(log2(N));
-		M = new int*[N];
-		REP(i, 0, N-1) {
-			M[i] = new int[LOGN];
-			memset(M[i], 0, LOGN * sizeof(int));
-		}
-		REP(i, 0, N-1) { M[i][0] = i; }
-		for (int j = 1; 1 << j <= N; j++) {
-		    for (int i = 0; i + (1 << j) - 1 < N; i++) {
-		        if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]]) {
-		            M[i][j] = M[i][j - 1];
-		        } else {
-		            M[i][j] = M[i + (1 << (j - 1))][j - 1];
-		        }
-		    }
-		}
-	}
-    int query(int i, int j) {
-        int k = log2(j - i + 1);
-        if(A[ M[i][k] ] <= A[ M[j - (1<<k) + 1][k] ]) { return M[i][k]; }
-        else { return M[j - (1<<k) + 1][k]; }
-    }
-};
-
-SparseTable ST(2*N, L);
 
 
 /*
@@ -242,16 +107,6 @@ a ^= b;
 b ^= a;
 a ^= b;
 
-if (num & 1)
-   cout << "ODD";
-else
-   cout << "EVEN";
-
- Use of strlen() can be avoided by:
-for (i=0; s[i]; i++)
-{
-}
-loop breaks when the character array ends.
 
 
 
