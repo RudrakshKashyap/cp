4cf08b9d1f658eda8413e41816c4a8945532a02c
Rudraksh Kashyap <200rudra@gmail.com>
2021-01-11 20:17:32 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,16 +1,16 @@
 // nCr
 //proof at https://cp-algorithms.com/algebra/module-inverse.html
 const int maxn = 6e5;
-ll facinv[maxn];
-ll inv[maxn];
-ll fac[maxn];
+ll facinv[maxn+1];
+ll inv[maxn+1];
+ll fac[maxn+1];
 
 void compute()
 {
     fac[0] = 1, fac[1] = 1;
     facinv[0] = 1, facinv[1] = 1;
     inv[1] = 1;
-    for(int i=2 ; i < maxn ; i++)
+    for(int i=2 ; i <= maxn ; i++)
     {
     	fac[i] = (i * fac[i-1]) % mod;
     	inv[i] = ((mod - mod/i) * inv[mod%i]) % mod;
