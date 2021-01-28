7a1def95f013b77b9cb3e88fe361ecfd413bb67f
Rudraksh Kashyap <200rudra@gmail.com>
2021-01-28 12:13:25 +0530

    Update and rename func in main.cpp to lambda_func.cpp

--- lambda_func.cpp
+++ lambda_func.cpp
@@ -33,6 +33,49 @@ auto dfs = [&](int u)
 };
 
 
+
+
+////custom comp for pq
+
+bool Compare(Node a, Node b);
+                                             //passing the type         //passing the actual function
+std::priority_queue<Node, std::vector<Node>, decltype(&Compare)> openSet(Compare);
+
+struct My_less
+{
+    auto operator()( int const a, int const b ) const
+        -> bool
+    { return a < b; }
+};
+
+std::priority_queue< int, std::vector<int>, My_less > pq;
+
+
+
+
+//can use struct also
+class comp{
+public:
+    int* b;
+    comp(vector<int>& a)
+    {
+        b = &a[0];
+    }
+    bool operator()(ar<int,2> o1, ar<int,2> o2)
+    {
+        return b[o1[0]]*b[o2[1]] > b[o2[0]]*b[o1[1]];
+    }
+};
+
+priority_queue<ar<int,2>, vector<ar<int,2>>, comp> pq(arr);
+
+
+
+
+
+
+
+
 //https://www.youtube.com/watch?v=9T8A89jgeTI&ab_channel=Computerphile
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
 template<class Fun>
