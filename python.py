cd9f83669bc5479def704a8bb71b7b01ea4c3ab5
Rudraksh Kashyap <hackerhaddi@gmail.com>
2021-08-19 20:04:23 +0530

    Update python.py

--- python.py
+++ python.py
@@ -234,3 +234,15 @@ li2 = copy.copy(li1)         li2 = li1.copy()       lst2 = lst1[:]
   
 # using deepcopy for deepcopy  
 li3 = copy.deepcopy(li1) 
+
+
+
+
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1
+
+# list[start:end:step]    end is not inclusive
+
+print(*my_list, sep = ", ", end = "") # for printing list elements
