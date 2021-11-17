caf256c5d32a808ce5d79f6ca98423076de7202c
Rudraksh Kashyap <hackerhaddi@gmail.com>
2021-11-17 11:40:27 +0530

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -35,6 +35,52 @@ ostream& operator << (ostream& out, __int128 num) {
   //   - converting num to string and doing out << res_string
   return out;
 }
+
+
+
+
+
+
+ostream& operator << (ostream &os, __int128 const& value){
+    static char buffer[64];
+    int index = 0;
+    __uint128_t T = (value < 0) ? (-(value + 1)) + __uint128_t(1) : value;
+    if (value < 0) 
+        os << '-';
+    else if (T == 0)
+        return os << '0';
+    for(; T > 0; ++index){
+        buffer[index] = static_cast<char>('0' + (T % 10));
+        T /= 10;
+    }    
+    while(index > 0)
+        os << buffer[--index];
+    return os;
+}
+istream& operator >> (istream& is, __int128& T){
+    static char buffer[64];
+    is >> buffer;
+    size_t len = strlen(buffer), index = 0;
+    T = 0; int mul = 1;
+    if (buffer[index] == '-')
+        ++index, mul *= -1;
+    for(; index < len; ++index)
+        T = T * 10 + static_cast<int>(buffer[index] - '0');
+    T *= mul;    
+    return is;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 	 
 	 
 #include <iostream>
