2945882474bc036b90b57c93948480252a128b76
CuteCow <hackerhaddi@gmail.com>
2025-02-28 17:04:21 +0530

    Create fundamentals.cpp

---
+++ fundamentals.cpp
@@ -0,0 +1,15 @@
+//https://cp-algorithms.com/algebra/euclid-algorithm.html
+int gcd(int a, int b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+            swap(a, b);
+        b -= a;
+    } while (b);
+    return a << shift;
+}
+
