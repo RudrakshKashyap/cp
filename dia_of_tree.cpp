137498d79974813bbea76c558a82f816ce399660
CuteCow <hackerhaddi@gmail.com>
2025-05-13 21:40:32 +0530

    Update dia_of_tree.cpp

--- dia_of_tree.cpp
+++ dia_of_tree.cpp
@@ -78,3 +78,53 @@ void dfs(int u, int p, int d)
     dfs(far, -1, 0); //2nd end point of diam
     cout << far_dist << ' ';
     // rep(i, n) cout << dist[i] << ' '; -- i think this is the dist from i to farthest node from it, that's why we needed 3dfs
+
+
+///////////////////
+https://codeforces.com/contest/2107/submission/318609473
+
+std::vector<bool> vis(n);
+    
+    std::vector<int> dis(n, -1), p(n);
+    std::vector<int> q;
+    auto bfs = [&](int x) {
+        q = {x};
+        dis[x] = 0;
+        p[x] = -1;
+        for (int i = 0; i < q.size(); i++) {
+            int x = q[i];
+            for (auto y : adj[x]) {
+                if (y != p[x] && !vis[y]) {
+                    dis[y] = dis[x] + 1;
+                    p[y] = x;
+                    q.push_back(y);
+                }
+            }
+        }
+        
+        int t = x;
+        for (auto x : q) {
+            if (dis[x] > dis[t] || (dis[x] == dis[t] && x > t)) {
+                t = x;
+            }
+        }
+        return t;
+    };
+    
+    auto work = [&](this auto &&self, int x) -> void {
+        int a = bfs(x);
+        int b = bfs(a);
+        ans.push_back({dis[b] + 1, std::max(a, b) + 1, std::min(a, b) + 1});
+        for (int i = b; i != -1; i = p[i]) {
+            vis[i] = true;
+        }
+        for (int i = b; i != -1; i = p[i]) {
+            for (auto x : adj[i]) {
+                if (!vis[x]) {
+                    self(x);
+                }
+            }
+        }
+    };
+    
+    work(0);
