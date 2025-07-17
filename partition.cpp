0bde0b3456c67199fe14884057fa6c606cc464e8
CuteCow <hackerhaddi@gmail.com>
2025-07-17 20:12:43 +0530

    Update partition.cpp

--- partition.cpp
+++ partition.cpp
@@ -8,16 +8,13 @@ E -> empty/not taken
 
 AT AT AT T T E T AT E AT T T E AT E E -> 3 possible choises
 
-
-rep(i,n)
+rep(oldmask, 1 << n)
 {
-    rep(oldmask, 1<<n)
+    int mask = oldmask ^ ((1 << n) - 1);
+    for(int newmask = mask; newmask >= 0; newmask = (newmask - 1) & mask)
     {
-        int mask = oldmask ^ ( (1<<n) - 1);
-        for(int newmask = mask; newmask >= 0; newmask = (newmask - 1)&mask)
-        {
-            // new mask will take elements which you left in old mask
-            dp[i+1][oldmask | newmask] = max(dp[i+1][oldmask | newmask], dp[i][oldmask] + sum);
-            if(newmask == 0) break;
-        }
+        // new mask will take elements which you left in old mask
+        dp[i+1][oldmask | newmask] = max(dp[i+1][oldmask | newmask], dp[i][oldmask] + sum);
+        if(newmask == 0) break;
     }
+}
